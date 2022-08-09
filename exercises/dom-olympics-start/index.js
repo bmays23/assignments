222f226dc2544e4bf4e139f3cdc6414104a02551
Brittany Mays <brttnymays@gmail.com>
2022-08-09 14:58:02 -0400

    vschool assignments

---
+++ exercises/dom-olympics-start/index.js
@@ -0,0 +1,12 @@
+var header = document.getElementById("header")
+header.textContent = "Javascript Made This!!"
+
+header.style.textAlign = "center"
+header.style.fontSize = "40px"
+
+header.innerHTML += "<div><span>Brittany</span> made this Javascript</div>"
+
+var text = document.querySelector("#first").textContent = "Let's Talk"
+var text = document.querySelector("#second").textContent = "Nope."
+var text = document.querySelector("#third").textContent = "You're no fun"
+var text = document.querySelector("#fourth").textContent = "And neither are you."
