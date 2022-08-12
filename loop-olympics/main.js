1d6dccc0795d2611c84b57d67c8a1ffc05c0f6ef
Brittany Mays <brttnymays@gmail.com>
2022-08-12 10:48:32 -0400

    loop olympics

---
+++ loop-olympics/main.js
@@ -0,0 +1,18 @@
+/*var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for(var i = 0; i < numbers.length; i++) {
+    console.log(numbers[i])
+}
+
+
+for(var i = numbers.length - 1; i >= 0; i--) {
+    console.log(numbers[i])
+}
+
+
+var fruit = ["banana", "orange", "apple", "kiwi"]
+
+for(var i = 0; i < fruit.length; i++) {
+    console.log(fruit[i])
+}
+*/
