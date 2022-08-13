39c91113604b73971c7bc643bf701872f3e25d94
Brittany Mays <brttnymays@gmail.com>
2022-08-13 09:47:15 -0400

    array methods

---
+++ array-methods/main.js
@@ -0,0 +1,25 @@
+var fruit = ["banana", "apple", "orange", "watermelon"];
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+
+//Remove the last item from the vegetable array.
+
+/*vegetables.pop()
+console.log(vegetables)
+*/
+
+//Remove the first item from the fruit array.
+
+/*fruit.shift()
+console.log(fruit)
+*/
+
+//Find the index of "orange."
+
+var orangeIndex = fruit.indexOf("orange")
+
+
+//Add that number to the end of the fruit array.
+
+fruit.push(orangeIndex)
+console.log(fruit)
+
