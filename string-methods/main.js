19b3859b08defe72aee9f182080bc1ad857bd6de
Brittany Mays <brttnymays@gmail.com>
2022-08-18 10:20:57 -0400

    updates to string

---
+++ string-methods/main.js
@@ -0,0 +1,15 @@
+var greeting = "hello"
+
+/*var upperGreeting = greeting.toUpperCase()
+var lowerGreeting = greeting.toLowerCase()
+console.log(upperGreeting + lowerGreeting)
+*/
+
+/*var length = greeting.length
+var newLength = length / 2
+console.log(newLength)
+*/
+
+//var firstHalf = greeting.slice(0,2)
+//console.log(firstHalf)
+
