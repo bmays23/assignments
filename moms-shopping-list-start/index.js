9843ab9c1d83b36bf81061de8d814025bc58d42b
Brittany Mays <brttnymays@gmail.com>
2022-08-22 13:18:56 -0400

    responsive color grid updates

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -6,17 +6,33 @@ form.addEventListener("submit", function(event) {
     form.title.value = ""
 
     var addToList = document.createElement("li")
-    addToList.textContent = item
+    var text = document.createElement("p")
+    text.textContent = item
     document.getElementById("list").append(addToList)
+    addToList.append(text)
 
     var editButton = document.createElement("button")
    
     editButton.textContent = "edit"
     editButton.style.backgroundColor = "cornflowerblue"
     editButton.style.borderColor = "cornflowerblue"
+    editButton.style.margin = "0px 0px 0px 3px"
     editButton.addEventListener("click", function() {
+        // console.log(addToList.textContent)
         var editItem = document.createElement("input");
+        editItem.value = text.textContent
         editItem.setAttribute("type", "text");
+        addToList.append(editItem)
+        var submit = document.createElement("button")
+        submit.textContent = "Submit"
+        addToList.append(submit)
+        submit.addEventListener("click", function() {
+            console.log("edit was submitted")
+            console.log(editItem.value)
+            text.textContent = editItem.value
+            submit.remove()
+            editItem.remove()
+        })
     })
 
     addToList.append(editButton)
@@ -26,10 +42,11 @@ form.addEventListener("submit", function(event) {
     XButton.textContent = "X"
     XButton.style.backgroundColor = "crimson"
     XButton.style.borderColor = "crimson"
+    XButton.style.margin = "3px"
     XButton.addEventListener("click", function() {
         addToList.remove();
-        XButton.remove();
-        editButton.remove();
+        // XButton.remove();
+        // editButton.remove();
     })
     addToList.append(XButton)
 
