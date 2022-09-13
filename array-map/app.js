889ce193f63c57691c90333de02dce4c52b4d9fc
Brittany Mays <brttnymays@gmail.com>
2022-09-12 23:25:36 -0400

    updates to array map

--- array-map/app.js
+++ array-map/app.js
@@ -22,7 +22,7 @@ console.log(string)
 
 //Capitalize the first letter of each name and make the rest of the characters lowercase
 
-const arr = ["ashley", "taylor", "kayla"]
+/*const arr = ["ashley", "taylor", "kayla"]
 
 const newArr = arr.map(function(user) {
     return user[0].toUpperCase() + user.substring(1);
@@ -30,3 +30,44 @@ const newArr = arr.map(function(user) {
 
 
 console.log(newArr)
+*/
+
+// Make an array of strings of the names
+
+/*const names = [
+    {name: "ashley" } ,
+    {name: "taylor"} ,
+    {name: "kayla"}
+
+]
+
+const string = names.map(function(name) {
+    return name.name
+})
+console.log(string)
+*/
+
+//Make an array of strings of the names saying whether or not they can go to The Matrix
+const users = [
+    {
+        name: "ashley",
+        age: 18
+    },
+    {
+        name: "taylor",
+        age: 10
+    },
+    {
+        name: "kayla",
+        age: 30
+    }
+]
+
+const enter = users.map(function(user) {
+    if (user.age >= 18)
+        return user.name + " can enter"
+    else if (user.age < 18)
+        return user.name + " cannot enter"
+})
+
+console.log(enter)
