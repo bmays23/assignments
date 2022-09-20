e9e26950ee6e2296d24162913dd7006fcf6bfdbf
Brittany Mays <brttnymays@gmail.com>
2022-09-20 10:17:08 -0400

    mario pest final

--- mario-pest/main.js
+++ mario-pest/main.js
@@ -5,12 +5,24 @@ form.addEventListener("submit", function(event) {
 
     const goom = form.amountOne.value
     const bomb = form.amountTwo.value
+    const cheep = form.amountThree.value
 
-    const total = parseInt(goom * 5) + parseInt(bomb * 7)
+    const total = parseInt(goom * 5) + parseInt(bomb * 7) + parseInt(cheep * 11)
 
     const input = document.createElement("div")
     input.textContent = total + " coins"
     input.style.fontSize = "50px"
     input.style.textAlign = "center"
     document.getElementsByName("body")[0].append(input)
+
+    const clearButton = document.getElementById("clear")
+
+    clearButton.addEventListener("click", function() {
+        input.remove()
+    })
+
+    
+
+
 })
+
