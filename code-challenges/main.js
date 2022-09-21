540cffb0575546e0b2a383a1bc9c7209f66caec9
Brittany Mays <brttnymays@gmail.com>
2022-09-21 16:08:27 -0400

    code challenges

---
+++ code-challenges/main.js
@@ -0,0 +1,8 @@
+//Create a function that takes a string and returns a new string with all vowels removed.
+
+const sentence = "My name is Brittany"
+
+const noVwl = sentence.replace("/[aeiou]/gi", "");
+
+console.log(noVwl)
+
