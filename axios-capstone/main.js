a199e6bdef1bf8b97b3409883482626ef67611b8
Brittany Mays <brttnymays@gmail.com>
2022-10-06 23:23:32 -0400

    axios updates

---
+++ axios-capstone/main.js
@@ -0,0 +1,40 @@
+//PART 1- GET
+
+const getTodo = axios.get("https://api.vschool.io/brittanymays/todo")
+.then(response => {listTodo(response.data)})
+.catch(error => {console.log(error)})
+
+
+
+function listTodo(data) {
+
+
+    for(let i = 0; i < data.length; i++){
+            const h1 = document.createElement('h1')
+            h1.textContent = data[i].title
+            document.body.appendChild(h1)
+        }
+
+}
+
+
+  
+
+//PART 2- POST
+
+const todoForm = document["todoform"]
+
+todoForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    
+    const newTodo = {
+        title: todoForm.title.value
+    }
+    
+    todoForm.title.value = ""
+    
+    axios.post("https://api.vschool.io/brittanymays/todo", newTodo)
+        .then(res => getTodo())
+        .catch(err => console.log(err))
+})
+
