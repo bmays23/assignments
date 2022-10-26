e5e92ac112e46907aa73e6a010a16bd8c99dc286
Brittany Mays <brttnymays@gmail.com>
2022-10-26 16:33:53 -0400

    more react practice

--- react-practice/main.js
+++ react-practice/main.js
@@ -4,23 +4,59 @@
     )
 }
 */
-const page = (
-    <div>
-        <img src="\B LOGO.png" width = "50px" />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on Github</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
+function Header() {
+    return (
+        <header>
+            <nav className = "nav">
+                <img src="\B LOGO.png" className = "image" />
+                <ul className = "nav-items">
+                    <li>About</li>
+                    <li>Pricing</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
 
-    </div>
-)
+}
+
+function Footer() {
+    return (
+        <footer className = "footer">
+            <small>© 2022 Mays development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function Body() {
+    return(
+        <div>
+            <h1 className = "header">Fun facts about React</h1>
+                <ul className = "main-list">
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100K stars on Github</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
+        </div>
+    )
+}
+
+function MyPage() {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+
+        </div>
+       
+    )
+}
 
 ReactDOM.render(
-    page, 
+    <MyPage />, 
     document.getElementById("root")
 )
 
