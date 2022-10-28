40a65f7ef85ef5a5a17c3d2695c21ce0a8f44ff9
Brittany Mays <brttnymays@gmail.com>
2022-10-28 16:13:32 -0400

    react stuff

---
+++ solo-project-one/solo-project-one/src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header>
+            <img className = "main=pic"/>
+            <h1>Main header</h1>
+        </header>
+    )
+}
