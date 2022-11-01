c7a7c392c8decda672311aa2e86055b9c5abc92f
Brittany Mays <brttnymays@gmail.com>
2022-11-01 16:01:54 -0400

    solo project one complete

--- solo-project-one/solo-project-one/src/App.jsx
+++ solo-project-one/solo-project-one/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Body from "./Body"
 import Header from "./Header"
+import Footer from "./Footer"
 
 export default function App() {
     return (
         <div className="container">
             <Header />
             <Body />
+            <Footer />
         </div>
     )
 }
--- solo-project-one/solo-project-one/src/Body.jsx
+++ solo-project-one/solo-project-one/src/Body.jsx
@@ -7,11 +7,13 @@ export default function Body() {
             <h2 className="job-title">Web Developer</h2>
             <h3>brittanymays.website</h3>
             <button className="button">Email</button>
-            <h2>About</h2>
-            <p>I am an aspiring web developer currently attending VSchool. 
-            </p>
-            <h2>Interests</h2>
-            <p>All things anime related. Food. Game of Thrones. House of the Dragon. Starbies.</p>
+            <div className="body-container">
+                <h2 className="about">About</h2>
+                <p className="first-paragraph">I am an aspiring web developer currently attending VSchool. 
+                </p>
+                <h2 className="interests">Interests</h2>
+                <p className="second-paragraph">All things anime related. Food. Game of Thrones. House of the Dragon. Starbies.</p>
+            </div>
         </main>
     )
 }
