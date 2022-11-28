1d4fabb86c0793cae4d67c9a522e1934e6568f86
Brittany Mays <brttnymays@gmail.com>
2022-11-28 13:13:35 -0500

    solo project 2 final

--- travel-card/travel-card/src/Location.jsx
+++ travel-card/travel-card/src/Location.jsx
@@ -3,8 +3,15 @@ import React from "react"
 export default function Location(props) {
     return (
         <div className="location">
-            <img src={props.item.imageUrl} className="pictures"/>
-
-
+            <div className="location--container">
+                <img src={props.item.imageUrl} className="pictures"/>
+                <div className="location--description">
+                    <h3>{props.item.location}</h3>
+                    <h1 className="title">{props.item.title}</h1>
+                    <h3>{props.item.startDate} - {props.item.endDate}</h3>
+                    <p>{props.item.description}</p>
+                    <line className="line"></line>
+                </div>
+            </div>
         </div>
     )}
