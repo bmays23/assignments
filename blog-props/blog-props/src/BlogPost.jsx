6b0c60348589e7eda5293638a45288be432f833f
Brittany Mays <brttnymays@gmail.com>
2022-11-29 15:42:44 -0500

    blog props final

--- blog-props/blog-props/src/BlogPost.jsx
+++ blog-props/blog-props/src/BlogPost.jsx
@@ -2,10 +2,13 @@ import React from "react"
 
 export default function BlogPost(props) {
     return (
-        <div className="list--item">
-            <h1 className="list--title">{props.item.title}</h1>
-            <h2 className="list--subTitle">{props.item.subTitle}</h2>
-            <h3 className="list--authorDate">Posted by {props.item.author} on {props.item.date}</h3>
+        <div>
+            <div className="list--item">
+                <h1 className="list--title">{props.item.title}</h1>
+                <h2 className="list--subTitle">{props.item.subTitle}</h2>
+                <h3 className="list--authorDate">Posted by {props.item.author} on {props.item.date}</h3>
+            </div>
+            <line></line>
         </div>
     )
 }
